3464ee21493cd26a4ded779d9826ab31162122d4
Clive Bunting <clive.bunting@googlemail.com>
2014-04-20 10:17:21 +0100

    Made the site more responsive

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,79 +1,83 @@
-$(document).ready(function(){
-	$.post('/generateRandomQuestionList', {
-		maxQuestionIndex: 179, 
-		numberOfQuestions: 10
-	}, 
-	function(data, status) {
-		viewModel.questionList(data.QuestionList);
-		viewModel.currentQuestion.indexOfWord(viewModel.questionList()[viewModel.questionNumber()]);
-		var a = viewModel.currentQuestion.indexOfWord();
-		$.post('/words', {
-			wordIndex: viewModel.currentQuestion.indexOfWord()
-		}, 
-		function(data, status) {
-			viewModel.currentQuestion.english(data.english);
-		});
+var QuestionAndAnswerModel = function () {
+    "use strict";
 
-	});
-});
-
-$("#inputItalian").on('keyup', function(e) {
-	if(e.keyCode==13 && viewModel.currentQuestion.italian().length > 0){
-		 $('#btnSubmit').trigger('click');
-	 }
-});
-
-$("#btnSubmit").click(function(){
-  $.post('/verbsAnswer',
-  {
-	Italian:viewModel.currentQuestion.italian,
-	Index:viewModel.currentQuestion.indexOfWord,
-	NextIndex:viewModel.questionNumber() + 1
-  },
-  function(data,status){
-	viewModel.addAnswer({question:data.question, answer:data.answer, correctAnswer:data.correctAnswer, correct:data.correct});
-	viewModel.currentQuestion.english(data.English);
-	viewModel.currentQuestion.italian("");
-	viewModel.currentQuestion.indexOfWord(data.Index);
-	viewModel.questionNumber(viewModel.questionNumber() + 1);
-  });
-});
-
-var QuestionAndAnswerModel = function() {
     var self = this;
-	self.questionList = ko.observableArray([]);
-	
-	self.questionNumber = ko.observable(0);
-	self.totalNumberOfQuestions = 10;
+    self.questionList = ko.observableArray([]);
 
-	self.currentQuestion = {};
-	self.currentQuestion.english = ko.observable("");
-	self.currentQuestion.italian = ko.observable("");
-	self.currentQuestion.indexOfWord = ko.observable(0);
+    self.questionNumber = ko.observable(0);
+    self.totalNumberOfQuestions = 10;
+
+    self.currentQuestion = {};
+    self.currentQuestion.english = ko.observable("");
+    self.currentQuestion.italian = ko.observable("");
+    self.currentQuestion.indexOfWord = ko.observable(0);
 
     self.answers = ko.observableArray([]);
-	
-    self.addAnswer = function(answer) {
+
+    self.addAnswer = function (answer) {
         self.answers.unshift(answer);
     };
- 
-    self.removeAnswer = function(answer) {
+
+    self.removeAnswer = function (answer) {
         self.answers.remove(answer);
     };
- 
-	self.correctAnswers = ko.computed(function() {
-        return ko.utils.arrayFilter(this.answers(), function(answer) {
+
+    self.correctAnswers = ko.computed(function () {
+        return ko.utils.arrayFilter(this.answers(), function (answer) {
             return answer.correct === true;
         });
     }, this);
-	
-	self.percentCorrect = ko.computed(function() {
-		if (this.answers().length === 0) return '0%';
-		return Math.floor((this.correctAnswers().length / this.answers().length) * 100) + '%'; 
-	}, this);
+
+    self.percentCorrect = ko.computed(function () {
+        if (this.answers().length === 0) { return '0%'; }
+        return Math.floor((this.correctAnswers().length / this.answers().length) * 100) + '%';
+    }, this);
 };
- 
+
 var viewModel = new QuestionAndAnswerModel();
+
+$(document).ready(function () {
+    "use strict";
+    $.post('/generateRandomQuestionList', {
+        maxQuestionIndex: 179,
+        numberOfQuestions: 10
+    },
+            function (data) {
+            viewModel.questionList(data.QuestionList);
+            viewModel.currentQuestion.indexOfWord(viewModel.questionList()[viewModel.questionNumber()]);
+            $.post('/words', {
+                wordIndex: viewModel.currentQuestion.indexOfWord()
+            },
+                function (data) {
+                    viewModel.currentQuestion.english(data.english);
+                });
+
+        });
+});
+
+$("#inputItalian").on('keyup', function (e) {
+    "use strict";
+    if (e.keyCode === 13 && viewModel.currentQuestion.italian().length > 0) {
+        $('#btnSubmit').trigger('click');
+    }
+});
+
+$("#btnSubmit").click(function () {
+    "use strict";
+    $.post('/verbsAnswer',
+        {
+            Italian: viewModel.currentQuestion.italian,
+            Index: viewModel.currentQuestion.indexOfWord,
+            NextIndex: viewModel.questionList()[viewModel.questionNumber() + 1]
+        },
+        function (data) {
+            viewModel.addAnswer({question: data.question, answer: data.answer, correctAnswer: data.correctAnswer, correct: data.correct});
+            viewModel.currentQuestion.english(data.English);
+            viewModel.currentQuestion.italian("");
+            viewModel.currentQuestion.indexOfWord(data.Index);
+            viewModel.questionNumber(viewModel.questionNumber() + 1);
+        });
+});
 
 // Activates knockout.js
 ko.applyBindings(viewModel);
--- routes/index.js
+++ routes/index.js
@@ -1,53 +1,66 @@
-/*
- * GET home page.
- */
 var random = require('random-to');
 
 var verbs = require('../data/verbs.json');
 
-exports.index = function(req, res){
-  res.render('index', { title: 'Italian Node' });
+exports.index = function (req, res) {
+    "use strict";
+    res.render('index', { title: 'Italian Node' });
 };
 
-exports.verbs = function(req, res){
-  res.render('verbs', { title: 'Italian Verbs' });
+exports.verbs = function (req, res) {
+    "use strict";
+    res.render('verbs', { title: 'Italian Verbs' });
 };
 
-exports.verbsAnswer = function(req, res){
-  if(verbs.verbs[req.body.Index].Italian === req.body.Italian.toLowerCase())
-  	res.send({
-	    correct:true,
-		question:verbs.verbs[req.body.Index].English,
-		answer:req.body.Italian, 
-		correctAnswer:verbs.verbs[req.body.Index].Italian,
-		English:verbs.verbs[req.body.NextIndex].English, 
-		Index:req.body.NextIndex
-		});
-  else
-  	res.send({
-	    correct:false,
-		question:verbs.verbs[req.body.Index].English,
-		answer:req.body.Italian, 
-		correctAnswer:verbs.verbs[req.body.Index].Italian,
-		English:verbs.verbs[req.body.NextIndex].English, 
-		Index:req.body.NextIndex
-		});
-};
+exports.verbsAnswer = function (req, res) {
+    "use strict";
 
-exports.words = function(req, res){
-  res.send({english:verbs.verbs[Number(req.body.wordIndex)].English});
-};
+    console.log(req.body.Index);
+    console.log(req.body.Italian.toLowerCase());
 
-exports.generateRandomQuestionList = function(req, res) {
-	var questions = [];
-    var count = Number(req.body.maxQuestionIndex) + 1;
-	
-    for (var i = count - req.body.numberOfQuestions; i < count; i++) {
-        var questionIndex = random.from0to(i + 1);
-        if (questions.indexOf(questionIndex) != -1)
-            questions.push(i);
-        else
-            questions.push(questionIndex);
+    if (verbs.verbs[req.body.Index].Italian === req.body.Italian.toLowerCase()) {
+        res.send({
+            correct: true,
+            question: verbs.verbs[req.body.Index].English,
+            answer: req.body.Italian,
+            correctAnswer: verbs.verbs[req.body.Index].Italian,
+            English: verbs.verbs[req.body.NextIndex].English,
+            Index: req.body.NextIndex
+        });
+    } else {
+        res.send({
+            correct: false,
+            question: verbs.verbs[req.body.Index].English,
+            answer: req.body.Italian,
+            correctAnswer: verbs.verbs[req.body.Index].Italian,
+            English: verbs.verbs[req.body.NextIndex].English,
+            Index: req.body.NextIndex
+        });
     }
-	res.send({QuestionList: questions});
+};
+
+exports.words = function (req, res) {
+    "use strict";
+    res.send({english: verbs.verbs[Number(req.body.wordIndex)].English});
+};
+
+exports.generateRandomQuestionList = function (req, res) {
+    "use strict";
+    var questions = [],
+        count = Number(req.body.maxQuestionIndex) + 1,
+        i,
+        questionIndex;
+
+    for (i = count - req.body.numberOfQuestions; i < count; i = i + 1) {
+        questionIndex = random.from0to(i + 1);
+        if (questions.indexOf(questionIndex) !== -1) {
+            questions.push(i);
+        } else {
+            questions.push(questionIndex);
+        }
+    }
+
+    console.log(questions);
+
+    res.send({QuestionList: questions});
 };
